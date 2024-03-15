7fb8e975bcf3dd55450ee4e9a5519211b1e8b243
Nicolas Garza <nicolas.garza@gmail.com>
2024-03-14 22:23:00 -0500

    kms

--- broadcast/main.go
+++ broadcast/main.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	n := maelstrom.NewNode()
 	var recieved_ints []int
+	var this_topology map[string][]string
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 
@@ -26,8 +27,40 @@ func main() {
 			return fmt.Errorf("message is not an integer")
 		}
 
+		send_data := map[string]any{
+			"type":    "broadcast",
+			"message": recieved_ints[len(recieved_ints)-1],
+		}
+		json_data, err := json.Marshal(send_data)
+		if err != nil {
+			return err
+		}
+
+		// fmt.Printf("this_topology: %v, this id: %s", this_topology, n.ID())
+		this_node := n.ID()
+		runeSlice := []rune(this_node)
+		var firstTwo string
+		if len(runeSlice) >= 2 {
+			firstTwo = string(runeSlice[:2])
+		}
+
+		// fmt.Printf("This node's neighbors: %s", this_topology[firstTwo])
+		for _, node_name := range this_topology[firstTwo] {
+			err := n.Send(node_name, json_data)
+			if err != nil {
+				return err
+			}
+		}
+
 		body["type"] = "broadcast_ok"
 		delete(body, "message")
+		jsonString, err := json.Marshal(body)
+		if err != nil {
+			log.Printf("Error logging map: %v", err)
+		} else {
+			log.Printf("Map contents: %s", jsonString)
+		}
+
 		return n.Reply(msg, body)
 	})
 
@@ -45,8 +78,38 @@ func main() {
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
-		body := make(map[string]any)
+		var body map[string]any
+
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		topologyData, ok := body["topology"].(map[string]interface{})
+		if !ok {
+			return fmt.Errorf("incorrect format for topology")
+		}
+
+		convertedTopology := make(map[string][]string)
+		for key, value := range topologyData {
+			if nodeList, ok := value.([]interface{}); ok {
+				var stringSlice []string
+				for _, node := range nodeList {
+					if nodeName, ok := node.(string); ok {
+						stringSlice = append(stringSlice, nodeName)
+					} else {
+						return fmt.Errorf("node name is not a string")
+					}
+				}
+				convertedTopology[key] = stringSlice
+			} else {
+				return fmt.Errorf("nodes are not a slice")
+			}
+		}
+
+		this_topology = convertedTopology
 		body["type"] = "topology_ok"
+		delete(body, "topology")
+
 		return n.Reply(msg, body)
 	})
 
