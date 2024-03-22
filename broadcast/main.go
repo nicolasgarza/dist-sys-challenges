376439bc6cdbfcf94d267a4331926b3c1ed2973d
Nicolas Garza <nicolas.garza@gmail.com>
2024-03-22 16:30:14 -0500

    I give up. I'm switching to buisness

--- broadcast/main.go
+++ broadcast/main.go
@@ -4,13 +4,21 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 func main() {
 	n := maelstrom.NewNode()
-	var recieved_ints []int
+
+	var (
+		recieved_ints []int
+		seen          map[int]bool
+		mu            sync.Mutex
+	)
+
+	seen = make(map[int]bool)
 	var this_topology map[string][]string
 	logfile, err := os.OpenFile("/Users/nicolasgarza/code/dist-sys-challenges/broadcast/debug.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 	if err != nil {
@@ -20,80 +28,77 @@ func main() {
 	log.SetOutput(logfile)
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
-		log.Println("In function broadcast")
-
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
 		if recieved_int, ok := body["message"].(float64); ok {
+			mu.Lock()
 			this_int := int(recieved_int)
-			recieved_ints = append(recieved_ints, this_int)
+			if !seen[this_int] {
+				seen[this_int] = true
+				recieved_ints = append(recieved_ints, this_int)
+				mu.Unlock()
+
+				go func() {
+					send_data := map[string]any{
+						"type":    "broadcast",
+						"message": int(this_int),
+					}
+					json_data, err := json.Marshal(send_data)
+					if err != nil {
+						log.Printf("Error marshaling send_data: %v", err)
+						return
+					}
+
+					for _, node_name := range this_topology[n.ID()] {
+						if node_name != n.ID() {
+							retries := 3
+							for i := 0; i < retries; i++ {
+								if err := n.RPC(node_name, json_data, func(_ maelstrom.Message) error {
+									return nil
+								}); err != nil {
+									log.Printf("Error sending to %s (attempt %d): %v", node_name, i+1, err)
+								} else {
+									break
+								}
+							}
+						}
+					}
+				}()
+			} else {
+				mu.Unlock()
+			}
 		} else {
 			log.Println("message is not an integer")
 		}
 
-		send_data := map[string]any{
-			"type":    "broadcast",
-			"message": recieved_ints[len(recieved_ints)-1],
-		}
-		json_data, err := json.Marshal(send_data)
-		if err != nil {
-			return err
-		}
-
-		this_node := n.ID()
-		runeSlice := []rune(this_node)
-		var firstTwo string
-		if len(runeSlice) >= 2 {
-			firstTwo = string(runeSlice[:2])
-		}
-
-		for _, node_name := range this_topology[firstTwo] {
-			if node_name == firstTwo {
-				continue
-			}
-			n.RPC(node_name, json_data, func(_ maelstrom.Message) error { return nil })
-		}
-
 		body["type"] = "broadcast_ok"
 		delete(body, "message")
-		jsonString, err := json.Marshal(body)
-		if err != nil {
-			log.Printf("Error logging map: %v", err)
-		} else {
-			log.Printf("Map contents: %s", jsonString)
-		}
-
 		return n.Reply(msg, body)
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
-		log.Println("In function read")
-
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
 		body["type"] = "read_ok"
+		mu.Lock()
 		body["messages"] = recieved_ints
-
+		mu.Unlock()
+		log.Println(recieved_ints)
 		return n.Reply(msg, body)
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
-		log.Println("In function topology")
-
 		var body map[string]interface{}
 
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
-		log.Println("--------------------------")
-		log.Println(body)
-		log.Println("--------------------------")
 
 		topologyInterface, exists := body["topology"]
 		if !exists {
